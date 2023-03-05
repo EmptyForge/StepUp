fad9d4df14299b7783108d34c66cf4a472db6442
Matt Tucker <MTucker421@gmail.com>
2023-03-04 23:41:15 -0500

    Multiplayer working with synchronized names and colors

---
+++ scripts/PlayerPanel.gd
@@ -0,0 +1,55 @@
+extends PanelContainer
+class_name PlayerPanel
+
+const player_panel_stylebox = preload("res://assets/theme/player_panel_stylebox.tres")
+
+@onready var player_inputs = $MarginContainer/PlayerInputs
+@onready var player_name_label = $MarginContainer/PlayerInputs/NameLabel
+@onready var player_text_entry = $MarginContainer/PlayerInputs/AnswerTextEdit
+@onready var player_submit_button = $MarginContainer/PlayerInputs/SubmitButton
+@onready var player_status_label = $MarginContainer/PlayerInputs/StatusLabel
+
+@onready var player_customizer = $MarginContainer/PlayerCustomization
+@onready var player_name_edit = $MarginContainer/PlayerCustomization/NameLineEdit
+@onready var player_colorpicker = $MarginContainer/PlayerCustomization/ColorPickerButton
+@onready var player_custom_submit = $MarginContainer/PlayerCustomization/CustomizationSubmitButton
+
+@onready var synchronizer = $MultiplayerSynchronizer
+
+@export var player_name : String = "Player"
+@export var player_color : Color = Color.DIM_GRAY
+@export var player_status : String = "Hello!"
+
+# Called when this instance of the scene enters the scene tree for the first time,
+# like when it's spawned by the multiplayer server
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
+# Called when the node enters the scene tree and is ready
+func _ready():
+	if not is_multiplayer_authority():
+		player_text_entry.hide()
+		player_submit_button.hide()
+		player_status_label.show()
+	else:
+		player_inputs.hide()
+		player_customizer.show()
+
+func _on_customization_submit_button_pressed():
+	player_name = player_name_edit.text
+	player_color = player_colorpicker.color
+	player_customizer.hide()
+	player_inputs.show()
+
+@rpc("any_peer")
+func update_visuals():
+	player_name_label.text = player_name
+	player_status_label.text = player_status
+	if player_color != self.get_theme_stylebox("panel").bg_color:
+		var new_stylebox = player_panel_stylebox.duplicate()
+		new_stylebox.bg_color = player_color
+		self.add_theme_stylebox_override("panel", new_stylebox)
+
+# Triggers whenever the multiplayer synchorizer sees that a synched property has changed
+func _on_multiplayer_synchronizer_synchronized():
+	update_visuals.rpc()
--- scripts/StepUp.gd
+++ scripts/StepUp.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var main_menu = $CanvasLayer/MainMenu
 @onready var connection_address_line_edit = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/ConnectionLineEdit
+@onready var game_client = $CanvasLayer/MultiplayerSpawnRoot/GameClient
+@onready var player_container = $CanvasLayer/MultiplayerSpawnRoot/GameClient/MarginContainer/VBoxContainer/PlayerContainer
+
+var PlayerPanel = preload("res://scenes/PlayerPanel.tscn")
 
 const PORT = 6288
 
@@ -15,21 +19,27 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _on_host_button_pressed():
 	main_menu.hide()
+	game_client.show()
 	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)  # Connect "peer_connected" signal to add_player()
 	
-	add_player(multiplayer.get_unique_id(), true)
+	add_host(multiplayer.get_unique_id())
 
 func _on_join_button_pressed():
 	main_menu.hide()
+	game_client.show()
 	
 	enet_peer.create_client("localhost", PORT)
 	multiplayer.multiplayer_peer = enet_peer
-	
-func add_player(peer_id:int, host:bool=false):
+
+func add_host(peer_id:int):
 	pass
+	
+func add_player(peer_id:int):
+	var player_panel = PlayerPanel.instantiate()
+	player_panel.name = str(peer_id)
+	player_container.add_child(player_panel)
